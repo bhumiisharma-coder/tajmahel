aa5471c5de46b2f523e65123dd72349af01e87e4
bhumiisharma-coder <bhumiisharma.2007@gmail.com>
2024-12-13 14:25:11 +0530

    Add files via upload

---
+++ tajmahel/j.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    $('.slider-events').slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    arrows:false,
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"></button>',
+    centerMode:true,
+    slidesToShow:4,
+    // slidesToScroll:2
+    });
+  };
+  
